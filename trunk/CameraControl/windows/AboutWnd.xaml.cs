209b8b486ca8ba20b04fab543b1e95261a84952e
dukusi@gmail.com <dukusi@gmail.com@16c0b2c2-ad78-a56d-5b77-55c3e44a0994>
2012-04-18 18:59:52 +0000

    About window

---
+++ trunk/CameraControl/windows/AboutWnd.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CameraControl.windows
+{
+  /// <summary>
+  /// Interaction logic for AboutWnd.xaml
+  /// </summary>
+  public partial class AboutWnd : Window
+  {
+    public AboutWnd()
+    {
+      InitializeComponent();
+    }
+
+    private void button2_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    private void button1_Click(object sender, RoutedEventArgs e)
+    {
+      System.Diagnostics.Process.Start("http://code.google.com/p/nikon-camera-control/");
+    }
+
+    private void button3_Click(object sender, RoutedEventArgs e)
+    {
+      System.Diagnostics.Process.Start("http://www.gnu.org/licenses/gpl-3.0.txt");
+    }
+  }
+}
