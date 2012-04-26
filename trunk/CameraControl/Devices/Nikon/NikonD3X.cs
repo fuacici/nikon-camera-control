8f2cbd911f1385d26270385e0c47f01f088bc6ef
dukusi@gmail.com <dukusi@gmail.com@16c0b2c2-ad78-a56d-5b77-55c3e44a0994>
2012-04-26 18:00:26 +0000

    Support for D3X

---
+++ trunk/CameraControl/Devices/Nikon/NikonD3X.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CameraControl.Devices.Classes;
+
+namespace CameraControl.Devices.Nikon
+{
+  class NikonD3X : NikonD5100
+  {
+    override public LiveViewData GetLiveViewImage()
+    {
+      LiveViewData viewData = new LiveViewData();
+      viewData.HaveFocusData = true;
+
+      const int headerSize = 64;
+
+      byte[] result = _stillImageDevice.ExecuteWithData(CONST_CMD_GetLiveViewImage);
+      if (result == null || result.Length <= headerSize)
+        return null;
+      int cbBytesRead = result.Length;
+      GetAditionalLIveViewData(viewData, result);
+
+      MemoryStream copy = new MemoryStream((int)cbBytesRead - headerSize);
+      copy.Write(result, headerSize, (int)cbBytesRead - headerSize);
+      copy.Close();
+      viewData.ImageData = copy.GetBuffer();
+
+      return viewData;
+    } 
+  }
+}
