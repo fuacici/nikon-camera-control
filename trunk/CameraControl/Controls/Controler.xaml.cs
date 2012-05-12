cffc8d3ecb9022bbc740023aec7da226c456dc79
dukusi@gmail.com <dukusi@gmail.com@16c0b2c2-ad78-a56d-5b77-55c3e44a0994>
2012-05-12 17:20:44 +0000

    Focus stacking (Part II)

---
+++ trunk/CameraControl/Controls/Controler.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace CameraControl.Controls
+{
+  /// <summary>
+  /// Interaction logic for Controler.xaml
+  /// </summary>
+  public partial class Controler : UserControl
+  {
+    public Controler()
+    {
+      InitializeComponent();
+    }
+  }
+}
