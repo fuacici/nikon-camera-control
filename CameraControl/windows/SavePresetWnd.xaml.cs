29abf7559eccbacc752ba602f8dae2c89f5ac6bc
dukusi <dukusi@gmail.com>
2012-08-23 19:02:50 +0000

    Improved multiple camera support (Part III)

---
+++ CameraControl/windows/SavePresetWnd.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CameraControl.Classes;
+
+namespace CameraControl.windows
+{
+  /// <summary>
+  /// Interaction logic for SavePresetWnd.xaml
+  /// </summary>
+  public partial class SavePresetWnd : Window
+  {
+    public CameraPreset CameraPreset { get; set; }
+
+    public SavePresetWnd(CameraPreset cameraPreset)
+    {
+      InitializeComponent();
+      CameraPreset = cameraPreset;
+      CameraPreset.BeginEdit();
+    }
+
+    private void btn_save_Click(object sender, RoutedEventArgs e)
+    {
+      CameraPreset.EndEdit();
+      DialogResult = true;
+      Close();
+    }
+
+    private void btn_cancel_Click(object sender, RoutedEventArgs e)
+    {
+      CameraPreset.CancelEdit(); 
+      Close();
+    }
+  }
+}
--- nsis/setup.nsi
+++ nsis/setup.nsi
@@ -5,7 +5,7 @@ Name NikonCameraControl
 
 # General Symbol Definitions
 !define REGKEY "SOFTWARE\$(^Name)"
-!define VERSION 0.7.199
+!define VERSION 0.7.200
 !define COMPANY "Duka Istvan"
 !define URL http://code.google.com/p/nikon-camera-control/
 
