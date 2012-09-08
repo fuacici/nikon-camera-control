deb47b4564fcb6e1611476c80c8362df96433ff0
dukusi <dukusi@gmail.com>
2012-09-08 08:38:36 +0000

    Thumbnail Manager (Part I)

---
+++ CameraControl/Classes/Queue/QueueItemFileItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CameraControl.Interfaces;
+
+namespace CameraControl.Classes.Queue
+{
+  public class QueueItemFileItem : IQueueItem
+  {
+    public FileItem FileItem { get; set; }
+
+    #region Implementation of IQueueItem
+
+    public bool Execute(QueueManager manager)
+    {
+      try
+      {
+        FileItem.GetExtendedThumb();
+      }
+      catch (Exception e)
+      {
+        ServiceProvider.Log.Error(e);
+      }
+
+      return true;
+    }
+
+    #endregion
+  }
+}
---
+++ CameraControl/Classes/QueueManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using CameraControl.Interfaces;
+
+namespace CameraControl.Classes
+{
+  public class QueueManager
+  {
+    public BlockingCollection<IQueueItem> Queue { get; set; }
+    private readonly BackgroundWorker _worker;
+    private object _locker = new object();
+
+
+    public QueueManager()
+    {
+      Queue =new BlockingCollection<IQueueItem>();
+      _worker = new BackgroundWorker();
+      _worker.DoWork += _worker_DoWork;
+    }
+
+    void _worker_DoWork(object sender, DoWorkEventArgs e)
+    {
+      while (true)
+      {
+        var item = Queue.Take();
+        if (!item.Execute(this))
+          break;
+      }
+    }
+
+
+    public void Add(IQueueItem item)
+    {
+      Queue.Add(item);
+      if(!_worker.IsBusy)
+        _worker.RunWorkerAsync();
+    }
+
+  }
+}
