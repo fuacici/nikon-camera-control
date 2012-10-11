62e1764afe46fc3a838407d0c31f3e62d3d52de2
dukusi <dukusi@gmail.com>
2012-10-11 17:57:12 +0000

    Simple localization system (Part II)

--- CameraControl/Translation/TranslationStrings.cs
+++ CameraControl/Translation/TranslationStrings.cs
@@ -2,6 +2,43 @@
 {
   public class TranslationStrings
   {
+    public static string Mode = "Mode";
+    public static string Iso = "_Iso";
+    public static string ShutterSpeed = "_Shutter speed";
+    public static string Aperture = "_Aperture";
+    public static string WhiteBalance = "_White Balance";
+    public static string ExposureComp = "_Exposure Comp.";
+    public static string Compression = "_Compression";
+    public static string MeteringMode = "_Metering Mode";
+    public static string FocusMode = "_Focus Mode";
+    public static string Battery = "Battery";
+    public static string SDRam = "SD_Ram";
+    public static string SDRamToolTip = "Capture images direct to PC without using card";
+    public static string CapturePhotoToolTip = "Capture photo";
+    public static string CapturePhotoNoAfToolTip = "Capture photo no auto focus";
+    public static string BraketingToolTip = "Braketing";
+    public static string SettingsToolTip = "Settings";
+    public static string TimeLapseToolTip = "Time lapse";
+    public static string FullscreenToolTip = "Fullscreen";
+    public static string LiveViewToolTip = "Live view";
+    public static string BrowseSessionsToolTip = "Browse sessions";
+    public static string SelectTagsToolTip = "Select Tags";
+    public static string AboutToolTip = "About";
+    public static string Session = "Session";
+    public static string SessionAdd = "Add";
+    public static string SessionEdit = "Edit";
+    public static string SessionDel = "Del";
+
+    public static string MainWindowTitle = "NCC";
+
+    public static string MsgPhotoTransferBegin = "Photo transfer begin";
+    public static string MsgPhotoTransferDone = "Photo transfer done";
+    public static string MsgPhotoTransferError = "Transfer error !\nMessage :{0}";
+    public static string MsgBulbModeNotSupported = "Bulb mode not supported !";
+    public static string MsgApplicationUpToDate = "Your application is up to date !";
+    public static string MsgUseSessionEditorTags = "Use session editor to define tags !";
+    public static string MsgLastSessionCantBeDeleted = "Last sesion can't be deleted";
+    public static string MsgDeleteSessionQuestion = "Do you want continue to deleting sesion {0} ?\nNo files will be deleted !";
 
     public static string MenuFile = "_File";
     public static string MenuReScan = "Rescan connected cameras";
