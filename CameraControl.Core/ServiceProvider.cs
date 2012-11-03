a52f9b3f095a8a1760ed7a6b9cf0fea9b5b41826
dukusi <dukusi@gmail.com>
2012-11-03 18:01:48 +0000

    New release

--- CameraControl.Core/ServiceProvider.cs
+++ CameraControl.Core/ServiceProvider.cs
@@ -6,10 +6,10 @@ namespace CameraControl.Core
 {
   public class ServiceProvider
   {
-    public static string AppName = "NikonCameraControl";
+    public static string AppName = "digiCamControl";
 
     public static Settings Settings { get; set; }
-    public static readonly log4net.ILog log = log4net.LogManager.GetLogger("NCC");
+    public static readonly log4net.ILog log = log4net.LogManager.GetLogger("digiCamControl");
     public static CameraDeviceManager DeviceManager { get; set; }
     public static TriggerClass Trigger { get; set; }
     public static WindowsManager WindowsManager { get; set; }
@@ -24,7 +24,7 @@ namespace CameraControl.Core
       ActionManager = new ActionManager();
       QueueManager = new QueueManager();
       Log.Debug("Application starting");
-      Log.Debug("Application verison" + System.Reflection.Assembly.GetEntryAssembly().GetName().Version);
+      Log.Debug("Application verison :" + System.Reflection.Assembly.GetEntryAssembly().GetName().Version);
     }
   }
 }
