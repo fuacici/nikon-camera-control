21cd9aeb5c7f5e4f44563c9a0a9a3d4d0e4dff0a
dukusi@gmail.com <dukusi@gmail.com@16c0b2c2-ad78-a56d-5b77-55c3e44a0994>
2012-11-19 17:06:12 +0000

    Some speed up of file transfer

--- trunk/PortableDeviceLib/StillImageDevice.cs
+++ trunk/PortableDeviceLib/StillImageDevice.cs
@@ -335,6 +335,195 @@ namespace PortableDeviceLib
       return res;
     }
 
+    public byte[] ExecuteReadBigDataWriteToFile(int code, int param1, int param2, TransferCallback callback, string fileName)
+    {
+      // source: http://msdn.microsoft.com/en-us/library/windows/desktop/ff384843(v=vs.85).aspx
+      // and view-source:http://www.experts-exchange.com/Programming/Languages/C_Sharp/Q_26860397.html
+      // error codes http://msdn.microsoft.com/en-us/library/windows/desktop/dd319335(v=vs.85).aspx
+      byte[] imgdate = new byte[8];
+
+      IPortableDeviceValues commandValues = (IPortableDeviceValues)new PortableDeviceTypesLib.PortableDeviceValuesClass();
+      IPortableDeviceValues pParameters = (IPortableDeviceValues)new PortableDeviceTypesLib.PortableDeviceValues();
+
+      IPortableDevicePropVariantCollection propVariant =
+        (IPortableDevicePropVariantCollection)new PortableDeviceTypesLib.PortableDevicePropVariantCollection();
+      IPortableDeviceValues pResults;
+
+      //commandValues.SetGuidValue(ref PortableDevicePKeys.WPD_PROPERTY_COMMON_COMMAND_CATEGORY, ref command.fmtid);
+      commandValues.SetGuidValue(PortableDevicePKeys.WPD_PROPERTY_COMMON_COMMAND_CATEGORY,
+                                       PortableDevicePKeys.WPD_COMMAND_MTP_EXT_EXECUTE_COMMAND_WITH_DATA_TO_READ.fmtid);
+      commandValues.SetUnsignedIntegerValue(PortableDevicePKeys.WPD_PROPERTY_COMMON_COMMAND_ID,
+                             PortableDevicePKeys.WPD_COMMAND_MTP_EXT_EXECUTE_COMMAND_WITH_DATA_TO_READ.pid);
+      commandValues.SetBufferValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_DATA, ref imgdate[0], (uint)imgdate.Length);
+
+
+      tag_inner_PROPVARIANT vparam1 = new tag_inner_PROPVARIANT();
+      tag_inner_PROPVARIANT vparam2 = new tag_inner_PROPVARIANT();
+      if (param1 > -1)
+      {
+        UintToPropVariant((uint)param1, out vparam1);
+        propVariant.Add(ref vparam1);
+      }
+      if (param2 > -1)
+      {
+        UintToPropVariant((uint)param2, out vparam2);
+        propVariant.Add(ref vparam2);
+      }
+      commandValues.SetIPortableDevicePropVariantCollectionValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_OPERATION_PARAMS, propVariant);
+      commandValues.SetUnsignedIntegerValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_OPERATION_CODE, (uint)code);
+
+      // According to documentation, first parameter should be 0 (see http://msdn.microsoft.com/en-us/library/dd375691%28v=VS.85%29.aspx)
+      this.portableDeviceClass.SendCommand(0, commandValues, out pResults);
+
+      try
+      {
+        int pValue = 0;
+        pResults.GetErrorValue(PortableDevicePKeys.WPD_PROPERTY_COMMON_HRESULT, out pValue);
+        if (pValue != 0)
+        {
+          // check if the device is busy, and after 100 ms seconds try again 
+          if (((uint)pValue) == PortableDeviceErrorCodes.ERROR_BUSY)
+          {
+            Thread.Sleep(50);
+            return ExecuteReadBigDataWriteToFile(code, param1, param2, callback, fileName);
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+      }
+      //string pwszContext = string.Empty;
+      //pResults.GetStringValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_CONTEXT, out pwszContext);
+      //uint cbReportedDataSize = 0;
+      //pResults.GetUnsignedIntegerValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_TOTAL_DATA_SIZE, out cbReportedDataSize);
+
+
+      uint tmpBufferSize = 0;
+      uint tmpTransferSize = 0;
+      string tmpTransferContext = string.Empty;
+      {
+        pResults.GetStringValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_CONTEXT, out tmpTransferContext);
+        pResults.GetUnsignedIntegerValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_TOTAL_DATA_SIZE, out tmpBufferSize);
+        pResults.GetUnsignedIntegerValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_OPTIMAL_TRANSFER_BUFFER_SIZE, out tmpTransferSize);
+
+        try
+        {
+          int pValue;
+          pResults.GetErrorValue(PortableDevicePKeys.WPD_PROPERTY_COMMON_HRESULT, out pValue);
+          if (pValue != 0)
+          {
+            return null;
+          }
+        }
+        catch
+        {
+        }
+      }
+
+      pParameters.Clear();
+      pResults.Clear();
+      uint offset = 0;
+      byte[] res = new byte[(int)tmpBufferSize];
+      bool cont = true;
+
+      var hFile = WinApi.CreateFile(fileName, (uint)WinApi.DesiredAccess.GENERIC_WRITE,
+                                   (uint)WinApi.ShareMode.FILE_SHARE_WRITE, IntPtr.Zero, (uint)WinApi.CreationDisposition.CREATE_ALWAYS,
+                                   (uint)WinApi.FlagsAndAttributes.FILE_ATTRIBUTE_NORMAL, IntPtr.Zero);
+
+
+      do
+      {
+        if (offset + tmpTransferSize >= tmpBufferSize)
+        {
+          cont = false;
+          tmpTransferSize = (uint)(tmpBufferSize - offset);
+        }
+
+        byte[] tmpData = new byte[(int)tmpTransferSize];
+        pParameters.SetGuidValue(PortableDevicePKeys.WPD_PROPERTY_COMMON_COMMAND_CATEGORY,
+                                 PortableDevicePKeys.WPD_COMMAND_MTP_EXT_READ_DATA.fmtid);
+        pParameters.SetUnsignedIntegerValue(ref PortableDevicePKeys.WPD_PROPERTY_COMMON_COMMAND_ID,
+                                            PortableDevicePKeys.WPD_COMMAND_MTP_EXT_READ_DATA.pid);
+        pParameters.SetStringValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_CONTEXT, tmpTransferContext);
+        pParameters.SetBufferValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_DATA, ref tmpData[0],
+                                   (uint)tmpTransferSize);
+        pParameters.SetUnsignedIntegerValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_NUM_BYTES_TO_READ,
+                                            (uint)tmpTransferSize);
+        pParameters.SetIPortableDevicePropVariantCollectionValue(
+          ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_OPERATION_PARAMS, propVariant);
+
+
+        portableDeviceClass.SendCommand(0, pParameters, out pResults);
+
+
+        uint cbBytesRead = 0;
+
+        try
+        {
+          int pValue = 0;
+          pResults.GetErrorValue(PortableDevicePKeys.WPD_PROPERTY_COMMON_HRESULT, out pValue);
+          if (pValue != 0)
+            return null;
+        }
+        catch (Exception ex)
+        {
+        }
+
+        callback((int)tmpBufferSize, (int)offset);
+
+        GCHandle pinnedArray = GCHandle.Alloc(imgdate, GCHandleType.Pinned);
+        IntPtr ptr = pinnedArray.AddrOfPinnedObject();
+
+        uint dataread = 0;
+        pResults.GetUnsignedIntegerValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_NUM_BYTES_READ,
+                                         out dataread);
+        pResults.GetBufferValue(ref PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_DATA, ptr, out cbBytesRead);
+
+        IntPtr tmpPtr = new IntPtr(Marshal.ReadInt64(ptr));
+
+        var natOverlap = new NativeOverlapped() { OffsetLow = (int)offset };
+
+        IntPtr written=new IntPtr();
+        WinApi.WriteFile(hFile, tmpPtr, (int)cbBytesRead, written, ref natOverlap);
+
+        //for (int i = 0; i < cbBytesRead; i++)
+        //{
+        //  res[offset + i] = Marshal.ReadByte(tmpPtr, i);
+        //}
+        Marshal.FreeHGlobal(tmpPtr);
+        pinnedArray.Free();
+
+        offset += cbBytesRead;
+      } while (cont);
+
+      WinApi.CloseHandle(hFile);
+
+      pParameters.Clear();
+      pResults.Clear();
+      {
+        pParameters.SetGuidValue(PortableDevicePKeys.WPD_PROPERTY_COMMON_COMMAND_CATEGORY, PortableDevicePKeys.WPD_COMMAND_MTP_EXT_END_DATA_TRANSFER.fmtid);
+        pParameters.SetUnsignedIntegerValue(ref PortableDevicePKeys.WPD_PROPERTY_COMMON_COMMAND_ID, PortableDevicePKeys.WPD_COMMAND_MTP_EXT_END_DATA_TRANSFER.pid);
+        pParameters.SetStringValue(PortableDevicePKeys.WPD_PROPERTY_MTP_EXT_TRANSFER_CONTEXT, tmpTransferContext);
+      }
+
+      portableDeviceClass.SendCommand(0, pParameters, out pResults);
+
+      try
+      {
+        int tmpResult = 0;
+
+        pResults.GetErrorValue(ref PortableDevicePKeys.WPD_PROPERTY_COMMON_HRESULT, out tmpResult);
+        if (tmpResult != 0)
+        {
+
+        }
+      }
+      catch
+      {
+      }
+      return res;
+    }
+
     public byte[] ExecuteReadData(int code)
     {
       return ExecuteReadData(code, -1, -1);
