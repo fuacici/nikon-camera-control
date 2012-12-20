f83d427bcf442cedbd3481317afe0c2330431bd0
dukusi@gmail.com <dukusi@gmail.com@16c0b2c2-ad78-a56d-5b77-55c3e44a0994>
2012-12-20 19:34:16 +0000

    Multiple live view support

---
+++ trunk/CameraControl/windows/LiveViewManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CameraControl.Classes;
+using CameraControl.Core;
+using CameraControl.Core.Classes;
+using CameraControl.Core.Interfaces;
+
+namespace CameraControl.windows
+{
+  public class LiveViewManager : IWindow
+  {
+    #region Implementation of IWindow
+
+    private Dictionary<object, LiveViewWnd> _register;
+
+    public LiveViewManager()
+    {
+      _register = new Dictionary<object, LiveViewWnd>();
+    }
+
+    public void ExecuteCommand(string cmd, object param)
+    {
+      switch (cmd)
+      {
+        case WindowsCmdConsts.LiveViewWnd_Show:
+          if (!_register.ContainsKey(param))
+          {
+            LiveViewWnd wnd = new LiveViewWnd();
+            ServiceProvider.Settings.ApplyTheme(wnd);
+            _register.Add(param, wnd);
+          }
+          _register[param].ExecuteCommand(cmd, param);
+          break;
+        case WindowsCmdConsts.LiveViewWnd_Hide:
+          if (_register.ContainsKey(param))
+            _register[param].ExecuteCommand(cmd, param);
+          break;
+        case CmdConsts.All_Close:
+          foreach (var liveViewWnd in _register)
+          {
+            liveViewWnd.Value.ExecuteCommand(cmd, param);
+          }
+          break;
+      }
+    }
+
+    public bool IsVisible { get; private set; }
+
+    #endregion
+  }
+}
