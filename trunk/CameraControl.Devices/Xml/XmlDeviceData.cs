822e6c413b87593f2e2ed809d1c1f43a42db6336
dukusi@gmail.com <dukusi@gmail.com@16c0b2c2-ad78-a56d-5b77-55c3e44a0994>
2013-04-06 18:05:46 +0000

    Application for test device MTP capabilities

---
+++ trunk/CameraControl.Devices/Xml/XmlDeviceData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CameraControl.Devices.Xml
+{
+    public class XmlDeviceData
+    {
+        public List<XmlCommandDescriptor> AvaiableCommands { get; set; }
+        public List<XmlEventDescriptor> AvaiableEvents { get; set; }
+        public List<XmlPropertyDescriptor> AvaiableProperties { get; set; }
+
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+
+
+        public XmlDeviceData()
+        {
+            AvaiableCommands = new List<XmlCommandDescriptor>();
+            AvaiableEvents = new List<XmlEventDescriptor>();
+            AvaiableProperties = new List<XmlPropertyDescriptor>();
+        }
+
+
+    }
+}
