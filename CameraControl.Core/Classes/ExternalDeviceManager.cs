800c92b78f2abc5487faa4dfc8d9c6de8d15e3a1
dukusi <dukusi@gmail.com>
2013-05-07 18:41:43 +0000

    External device support (Part I)

---
+++ CameraControl.Core/Classes/ExternalDeviceManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CameraControl.Core.Interfaces;
+using CameraControl.Devices.Classes;
+
+namespace CameraControl.Core.Classes
+{
+    /// <summary>
+    /// Manager for external device handing like external shutter release
+    /// </summary>
+    public class ExternalDeviceManager
+    {
+        public AsyncObservableCollection<IExternalShutterReleaseSource> ExternalShutterReleaseSources { get; set; }
+
+        public ExternalDeviceManager()
+        {
+            ExternalShutterReleaseSources = new AsyncObservableCollection<IExternalShutterReleaseSource>();
+        }
+    }
+}
---
+++ CameraControl.Core/Interfaces/IExternalShutterReleaseSource.cs
@@ -0,0 +1,11 @@
+﻿using CameraControl.Core.Classes;
+
+namespace CameraControl.Core.Interfaces
+{
+    public interface IExternalShutterReleaseSource
+    {
+        string Name { get; set; }
+        bool Execute(CustomConfig config);
+        bool CanExecute(CustomConfig config);
+    }
+}
