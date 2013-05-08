91e5b0980787bca1c231f17cb42bef26b98c4281
dukusi <dukusi@gmail.com>
2013-05-08 20:04:06 +0000

    External device support (Part III)

--- CameraControl.Core/Classes/CustomConfig.cs
+++ CameraControl.Core/Classes/CustomConfig.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Controls;
+using System.Xml.Serialization;
+using CameraControl.Core.Interfaces;
 using CameraControl.Devices.Classes;
 
 namespace CameraControl.Core.Classes
@@ -10,7 +13,20 @@ namespace CameraControl.Core.Classes
     {
         public string Name { get; set; }
         public AsyncObservableCollection<ValuePair> ConfigData { get; set; }
-        public string DriverName { get; set; }
+        private string _driverName;
+        public string DriverName
+        {
+            get { return _driverName; }
+            set
+            {
+                _driverName = value;
+                IExternalShutterReleaseSource source = ServiceProvider.ExternalDeviceManager.Get(_driverName);
+                Config = source.GetConfig(this);
+            }
+        }
+
         public SourceEnum SourceEnum { get; set; }
+        [XmlIgnore]
+        public UserControl Config { get; set; }
     }
 }
