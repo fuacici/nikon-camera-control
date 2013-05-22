4ca3a7d70db7c3f65de668d6c944ac3df51ba149
dukusi@gmail.com <dukusi@gmail.com@16c0b2c2-ad78-a56d-5b77-55c3e44a0994>
2013-05-22 20:44:02 +0000

    External device support (Part 8)

---
+++ trunk/CameraControl.Core/Scripting/IScriptCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Xml;
+using CameraControl.Core.Classes;
+
+namespace CameraControl.Core.Scripting
+{
+    public interface IScriptCommand
+    {
+        bool Execute();
+        XmlNode Save();
+        void Load(XmlNode node);
+        bool IsExecuted { get; set; }
+        bool Executing { get; set; }
+        string Name { get; set; }
+        string DisplayName { get; set; }
+        UserControl GetConfig();
+    }
+}
---
+++ trunk/CameraControl.Core/Scripting/ScriptManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CameraControl.Core.Scripting.ScriptCommands;
+
+namespace CameraControl.Core.Scripting
+{
+    public class ScriptManager
+    {
+        public List<IScriptCommand> AvaiableCommands { get; set; }
+
+        public ScriptManager()
+        {
+            AvaiableCommands=new List<IScriptCommand> {new BulbCapture()};
+        }
+    }
+}
