2dd0e758a4f6591fb6eec6a311af25cf3ed483a6
dukusi <dukusi@gmail.com>
2013-05-26 09:18:19 +0000

    0000042: Focus staking some time fail

--- CameraControl.Devices/Nikon/NikonBase.cs
+++ CameraControl.Devices/Nikon/NikonBase.cs
@@ -839,14 +839,13 @@ namespace CameraControl.Devices.Nikon
         {
             if (step == 0)
                 return;
-            lock (Locker)
-            {
+            //lock (Locker)
+            //{
                 //DeviceReady();
                 ErrorCodes.GetException(step > 0
                                           ? ExecuteWithNoData(CONST_CMD_MfDrive, 0x00000001, (uint)step)
                                           : ExecuteWithNoData(CONST_CMD_MfDrive, 0x00000002, (uint)-step));
-                DeviceReady();
-            }
+            //}
         }
 
         public override void AutoFocus()
