df768de6113dc4482c4cd15cd9020a41f330c5b0
dukusi@gmail.com <dukusi@gmail.com@16c0b2c2-ad78-a56d-5b77-55c3e44a0994>
2013-06-17 17:22:59 +0000

    0000050: Template element for file extension in session file name template

--- trunk/CameraControl.Core/Translation/TranslationStrings.cs
+++ trunk/CameraControl.Core/Translation/TranslationStrings.cs
@@ -91,7 +91,7 @@ namespace CameraControl.Core.Translation
         public static string LabelSessonName = "Session name :";
         public static string LabelFolder = "Folder :";
         public static string LabelFileNameTemplate = "File Name Template :";
-        public static string LabelFileNameTemplateToolTip = "$N - Session name &#xa;$C - Counter &#xa;$E - Exposure comp &#xa;$X - Camera name&#xa;$D - Current date&#xa;$Tag1 - Selected tag 1&#xa;$Tag2 - Selected tag 2&#xa;$Tag3 - Selected tag 3&#xa;$Tag4 - Selected tag 4";
+        public static string LabelFileNameTemplateToolTip = "$N - Session name &#xa;$C - Counter &#xa;$E - Exposure comp &#xa;$X - Camera name&#xa;$D - Current date&#xa;$Type - Type of file (Raw|Jpg) &#xa;$Tag1 - Selected tag 1&#xa;$Tag2 - Selected tag 2&#xa;$Tag3 - Selected tag 3&#xa;$Tag4 - Selected tag 4";
         public static string LabelCounter = "Counter";
         public static string LabelUseOriginal = "Use original filename gave by the camera";
         public static string LabelUseOriginalToolTip = "This option isn't works with WIA driver";
