3c91b8637e01db915a4827ae55ae3d5d6ef7938b
dukusi <dukusi@gmail.com>
2013-08-04 20:29:50 +0000

    Refresh directory three after every tree opening again

--- CameraControl/Controls/FolderBrowser.xaml.cs
+++ CameraControl/Controls/FolderBrowser.xaml.cs
@@ -7,118 +7,124 @@ using System.Windows.Input;
 
 namespace CameraControl.Controls
 {
-  /// <summary>
-  /// Interaction logic for FolderBrowser.xaml
-  /// </summary>
-  public partial class FolderBrowser : UserControl, INotifyPropertyChanged
-  {
-    public new event MouseButtonEventHandler MouseDoubleClick;
-
-    private object dummyNode = null;
-
-    private string _selectedImagePath;
-    public string SelectedImagePath
+    /// <summary>
+    /// Interaction logic for FolderBrowser.xaml
+    /// </summary>
+    public partial class FolderBrowser : UserControl, INotifyPropertyChanged
     {
-      get { return _selectedImagePath; }
-      set
-      {
-        _selectedImagePath = value;
-        NotifyPropertyChanged("SelectedImagePath");
-      }
-    }
+        public new event MouseButtonEventHandler MouseDoubleClick;
 
-    public FolderBrowser()
-    {
-      InitializeComponent();
-    }
+        private object dummyNode = null;
 
-    private void UserControl_Loaded(object sender, RoutedEventArgs e)
-    {
-      if (foldersItem.Items.Count > 0)
-        return;
-      foreach (string s in Directory.GetLogicalDrives())
-      {
-        TreeViewItem item = new TreeViewItem();
-        item.Header = s;
-        item.Tag = s;
-        item.FontWeight = FontWeights.Normal;
-        item.Items.Add(dummyNode);
-        item.Expanded += folder_Expanded;
-        foldersItem.Items.Add(item);
-      }
-    }
-
-    void folder_Expanded(object sender, RoutedEventArgs e)
-    {
-      TreeViewItem item = (TreeViewItem)sender;
-      //if (item.Items.Count == 1 && item.Items[0] == dummyNode)
-      //{
-        item.Items.Clear();
-        try
+        private string _selectedImagePath;
+        public string SelectedImagePath
         {
-          foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
-          {
-            TreeViewItem subitem = new TreeViewItem();
-            subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
-            subitem.Tag = s;
-            subitem.FontWeight = FontWeights.Normal;
-            subitem.Items.Add(dummyNode);
-            subitem.Expanded += folder_Expanded;
-            item.Items.Add(subitem);
-          }
+            get { return _selectedImagePath; }
+            set
+            {
+                _selectedImagePath = value;
+                NotifyPropertyChanged("SelectedImagePath");
+            }
         }
-        catch (Exception) { }
-      //}
-    }
 
-    private void foldersItem_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-    {
-      TreeView tree = (TreeView)sender;
-      TreeViewItem temp = ((TreeViewItem)tree.SelectedItem);
-
-      if (temp == null)
-        return;
-      SelectedImagePath = "";
-      string temp1 = "";
-      string temp2 = "";
-      while (true)
-      {
-        temp1 = temp.Header.ToString();
-        if (temp1.Contains(@"\"))
+        public FolderBrowser()
         {
-          temp2 = "";
+            InitializeComponent();
         }
-        SelectedImagePath = temp1 + temp2 + SelectedImagePath;
-        if (temp.Parent.GetType().Equals(typeof(TreeView)))
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-          break;
+            if (foldersItem.Items.Count > 0)
+                return;
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = s;
+                item.Tag = s;
+                item.FontWeight = FontWeights.Normal;
+                item.Items.Add(dummyNode);
+                foldersItem.Items.Add(item);
+                item.Expanded += folder_Expanded;
+            }
         }
-        temp = ((TreeViewItem)temp.Parent);
-        temp2 = @"\";
-      }
-      //show user selected path
-      //MessageBox.Show(SelectedImagePath);
+
+        void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                TreeViewItem item = (TreeViewItem) sender;
+                string[] folders = Directory.GetDirectories(item.Tag.ToString());
+                if ((item.Items.Count == 1 && item.Items[0] == dummyNode) || item.Items.Count != folders.Length)
+                {
+                    item.Items.Clear();
+                    foreach (string s in folders)
+                    {
+                        TreeViewItem subitem = new TreeViewItem();
+                        subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                        subitem.Tag = s;
+                        subitem.FontWeight = FontWeights.Normal;
+                        subitem.Items.Add(dummyNode);
+                        subitem.Expanded += folder_Expanded;
+                        item.Items.Add(subitem);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+
+            }
+           
+        }
+
+        private void foldersItem_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            TreeView tree = (TreeView)sender;
+            TreeViewItem temp = ((TreeViewItem)tree.SelectedItem);
+
+            if (temp == null)
+                return;
+            SelectedImagePath = "";
+            string temp1 = "";
+            string temp2 = "";
+            while (true)
+            {
+                temp1 = temp.Header.ToString();
+                if (temp1.Contains(@"\"))
+                {
+                    temp2 = "";
+                }
+                SelectedImagePath = temp1 + temp2 + SelectedImagePath;
+                if (temp.Parent.GetType().Equals(typeof(TreeView)))
+                {
+                    break;
+                }
+                temp = ((TreeViewItem)temp.Parent);
+                temp2 = @"\";
+            }
+            //show user selected path
+            //MessageBox.Show(SelectedImagePath);
+        }
+
+        #region Implementation of INotifyPropertyChanged
+
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #endregion
+
+        private void foldersItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (MouseDoubleClick != null)
+                MouseDoubleClick(sender, e);
+        }
+
     }
-
-    #region Implementation of INotifyPropertyChanged
-
-    public virtual event PropertyChangedEventHandler PropertyChanged;
-
-    public virtual void NotifyPropertyChanged(String info)
-    {
-      if (PropertyChanged != null)
-      {
-        PropertyChanged(this, new PropertyChangedEventArgs(info));
-      }
-    }
-
-    #endregion
-
-    private void foldersItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-    {
-      if (MouseDoubleClick != null)
-        MouseDoubleClick(sender, e);
-    }
-
-  }
 }
