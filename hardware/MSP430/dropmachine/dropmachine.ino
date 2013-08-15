153e5d098ffe51c8a7fa042a4e10b9618c10a4a9
dukusi <dukusi@gmail.com>
2013-08-15 21:13:23 +0000

    Fixed water drop plugin

--- hardware/MSP430/dropmachine/dropmachine.ino
+++ hardware/MSP430/dropmachine/dropmachine.ino
@@ -1,21 +1,4 @@
-/*
-  Serial Event example
- 
- When new serial data arrives, this sketch adds it to a String.
- When a newline is received, the loop prints the string and 
- clears it.
- 
- A good test for this is to try it with a GPS receiver 
- that sends out NMEA 0183 sentences. 
- 
- Created 9 May 2011
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/SerialEvent
- 
- */
+
 
 String inputString = "";         // a string to hold incoming data
 String command = "";
@@ -38,11 +21,13 @@ int drop2_wait_time = 100;
 int drop3_time = 45;
 int flash_time = 370;
 // -----------------------
+  bool shouldBlink = false;  
+// -----------------------
 void setup() {
   // initialize serial:
   Serial.begin(9600);
   // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
+  inputString.reserve(300);
   pinMode(valvePin, OUTPUT);
   pinMode(cameraPin, OUTPUT);
   pinMode(ledPin, OUTPUT);  
@@ -53,6 +38,11 @@ void setup() {
 
 void loop() {
 
+  if(shouldBlink)
+  {
+    blinkLed();
+    shouldBlink = false;
+  }
   if (digitalRead(buttonPin) == LOW)
   {
     start = true;
@@ -61,36 +51,9 @@ void loop() {
   }
   // print the string when a newline arrives:
   if (start) {
+    resetState();
     start = false;
-    digitalWrite(cameraPin, HIGH);
-    delay(camera_timer/2);
-    digitalWrite(valvePin, HIGH);
-    delay(drop1_time);    
-    digitalWrite(valvePin, LOW);    
- 
-    delay(drop_wait_time);        
-    digitalWrite(valvePin, HIGH);
-    delay(drop2_time);    
-    digitalWrite(valvePin, LOW);   
-
-    delay(drop2_wait_time);        
-
-    /*
-    Trigger the 3 drop only if size >0
-    */
-    if(drop3_time>0){
-          digitalWrite(valvePin, HIGH);
-          delay(drop3_time);    
-          digitalWrite(valvePin, LOW);   
-    }
-
-    delay(flash_time-drop1_time-drop_wait_time-drop2_time-drop2_wait_time-drop3_time );    
-    digitalWrite(flashPin, HIGH);
-    delay(100);    
-    digitalWrite(flashPin, LOW);    
-    delay(camera_timer/2);
-
-    digitalWrite(cameraPin, LOW);
+    doWaterDrop();
   }
 }
 
@@ -101,12 +64,11 @@ void loop() {
  response.  Multiple bytes of data may be available.
  */
 void serialEvent() {
-    bool shouldBlink = false;  
-  while (Serial.available()) {
+  while ( Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read(); 
     inputString += inChar;
-    
+
     if(inChar == ' ')
     {
       command = "";
@@ -115,20 +77,21 @@ void serialEvent() {
       shouldBlink = true;
     }
 
-    if(inChar == '>')
+    if(inChar == '|')
     {
-      digitalWrite(valvePin, HIGH);
-      delay(500);    
-      digitalWrite(valvePin, LOW); 
+     digitalWrite(valvePin, HIGH);
+    }    
+
+    if(inChar == '\\')
+    {
+     digitalWrite(valvePin, LOW);
     }    
 
     if(inChar == '<')
     {
-      digitalWrite(valvePin, HIGH);
-      delay(drop1_time);    
-      digitalWrite(valvePin, LOW);
+      stPin(valvePin, drop1_time);
     }    
-    
+
     if(inChar == '?')
     {
       command = "";
@@ -141,58 +104,15 @@ void serialEvent() {
     {
       command = inputString;
       inputString = "";
-      Serial.println(command);
     }
 
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
       stringComplete = true;      
-              Serial.println(inputString);
-      if(command=="c=")
-      {
-        camera_timer = inputString.toInt();
-        shouldBlink = true;
-      } 
-      if(command=="d1=")
-      {
-        drop1_time = inputString.toInt();
-        shouldBlink = true;
-      } 
-      if(command=="dw=")
-      {
-        drop_wait_time = inputString.toInt();
-        shouldBlink = true;
-      } 
-      if(command=="dw2=")
-      {
-        drop2_wait_time = inputString.toInt();
-        shouldBlink = true;
-      }       
-      if(command=="d2=")
-      {
-        drop2_time = inputString.toInt();
-        shouldBlink = true;
-      } 
-      if(command=="d3=")
-      {
-        drop3_time = inputString.toInt();
-        shouldBlink = true;
-      }      
-      if(command=="f=")
-      {
-        flash_time = inputString.toInt();
-        shouldBlink = true;
-      } 
-      command = "";
-      inputString = "";
+      phraseParams();
     } 
   }
-      if(shouldBlink)
-      {
-        blinkLed();
-        shouldBlink = false;
-      }
 }
 
 void resetState(){
@@ -202,41 +122,111 @@ void resetState(){
   digitalWrite(ledPin, LOW);
 }
 
+void phraseParams()
+{
+  if(command=="c=")
+  {
+    camera_timer = inputString.toInt();
+//    shouldBlink = true;
+  } 
+  if(command=="d1=")
+  {
+    drop1_time = inputString.toInt();
+//    shouldBlink = true;
+  } 
+  if(command=="dw=")
+  {
+    drop_wait_time = inputString.toInt();
+//    shouldBlink = true;
+  } 
+  if(command=="dw2=")
+  {
+    drop2_wait_time = inputString.toInt();
+//    shouldBlink = true;
+  }       
+  if(command=="d2=")
+  {
+    drop2_time = inputString.toInt();
+//    shouldBlink = true;
+  } 
+  if(command=="d3=")
+  {
+    drop3_time = inputString.toInt();
+//    shouldBlink = true;
+  }      
+  if(command=="f=")
+  {
+    flash_time = inputString.toInt();
+    shouldBlink = true;
+  } 
+  command = "";
+  inputString = "";  
+}
+
+
 void blinkLed()
 {
-  for (int i=0; i <= 1; i++){
-    digitalWrite(ledPin, HIGH);
-    delay(25);
-    digitalWrite(ledPin, LOW);
-    delay(25);    
-  }
+ // for (int i=0; i <= 1; i++){
+    stPin(ledPin, 25);
+//  }
 }
 
 void sendData()
 {
   Serial.print("camera_timer=");
   Serial.println(camera_timer);
-  
+
   Serial.print("drop1_time=");
   Serial.println(drop1_time);
-  
+
   Serial.print("drop_wait_time=");
   Serial.println(drop_wait_time);
-  
+
   Serial.print("drop2_wait_time=");
   Serial.println(drop2_wait_time);  
-  
+
   Serial.print("drop2_time=");
   Serial.println(drop2_time);
-  
+
   Serial.print("drop3_time=");
   Serial.println(drop3_time);  
-  
+
   Serial.print("flash_time=");
   Serial.println(flash_time);
 }
 
+void doWaterDrop()
+{
+    digitalWrite(cameraPin, HIGH);
 
+    delay(camera_timer/2);
+    stPin(valvePin, drop1_time);
+
+    delay(drop_wait_time);        
+    stPin(valvePin, drop2_time);
+
+    delay(drop2_wait_time);        
+
+    /*
+    Trigger the 3 drop only if size >0
+     */
+    if(drop3_time>0){
+      stPin(valvePin, drop3_time);
+    }
+
+    delay(flash_time-drop1_time-drop_wait_time-drop2_time-drop2_wait_time-drop3_time );    
+    stPin(flashPin, 100);
+    delay(camera_timer/2);
+
+    digitalWrite(cameraPin, LOW);  
+}
+
+void stPin(int pin, int delayM)
+{
+    digitalWrite(pin, HIGH);
+    delay(delayM);    
+    digitalWrite(pin, LOW);     
+}
 
 
 
