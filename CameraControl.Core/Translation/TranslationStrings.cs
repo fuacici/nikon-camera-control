8a5014ae56d89b802055ab1ea597b8d9b752e74d
dukusi <dukusi@gmail.com>
2013-09-22 19:31:59 +0000

    0000066: "Clear All" button on the Bracketing window

--- CameraControl.Core/Translation/TranslationStrings.cs
+++ CameraControl.Core/Translation/TranslationStrings.cs
@@ -381,5 +381,6 @@ namespace CameraControl.Core.Translation
         public static string LabelLiveViewRotation = "Live view rotation";
         public static string LabelAutomaticallyGuiding = "Automatically PHD Guiding after photo captured";
         public static string LabelAutoRotate = "Auto rotate";
+        public static string LabelReset = "Reset";
     }
 }
