e97ae22d0007a10cf69f8ccd5818a940217036c2
dukusi <dukusi@gmail.com>
2014-03-22 20:07:03 +0000

    New webserver implementation part II

--- CameraControl.Application/WebServer/Script.js
+++ CameraControl.Application/WebServer/Script.js
@@ -3,7 +3,7 @@
     var xmlHttp = null;
 
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.open( "GET", theUrl, true );
     xmlHttp.send( null );
     return xmlHttp.responseText;
   }
